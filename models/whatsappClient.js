dbf441d837afe17dab49ac2564cf48b5e1e95bd7
Bruno Wladimir <brunowladimir@MacBook-Air-de-Bruno.local>
2024-02-12 12:14:05 -0300

    pupteer atualizado

--- models/whatsappClient.js
+++ models/whatsappClient.js
@@ -5,12 +5,17 @@ const { create } = require('puppeteer');
 const client = new Client({
   puppeteer: {
     headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
   },
   authStrategy: new LocalAuth({
     clientId: "553199631088"
   })
 });
-
+// const client = new Client(
+//   { puppeteer: { 
+//     headless: true,
+//     args: ['--no-sandbox', '--disable-setuid-sandbox']}, 
+//     session: sessionCfg });
 
 
 
