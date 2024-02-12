6f12aeafb478538b63d9c66be1213e1e1b1eb3f8
Bruno Wladimir <brunowladimir@MacBook-Air-de-Bruno.local>
2024-02-12 08:17:17 -0300

    setandon numero zap manual

--- routes/loja.js
+++ routes/loja.js
@@ -311,8 +311,8 @@ console.log("te cliente : "+ req.body.telefone_cliente)
     //await Sorteio.create(dadosLoja)
     const link  = await  gerar_link(email,telefone_cliente);
 
-    await sendzapfunction(req.body.telefone_cliente,link); //aqui manda a mensagem para o clinte
-
+    //await sendzapfunction(req.body.telefone_cliente,link); //aqui manda a mensagem para o clinte
+    await client.sendMessage("553199631088@c.us", "Ola ");
     res.status(200).json({ message: "Cadastrado com sucesso na promoção " });
 
   }
