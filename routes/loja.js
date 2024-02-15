066ff807099fc0edf1074dc095848f2dc8390e9b
Bruno Wladimir <brunowladimir@MacBook-Air-de-Bruno.local>
2024-02-14 21:25:39 -0300

    adcao de dominio

--- routes/loja.js
+++ routes/loja.js
@@ -600,7 +600,7 @@ console.log("numero preparado"+ serialize)
  
     if (serialize) {
 
-      const imageBuffer = fs.readFileSync('images/panela.png');
+      const imageBuffer = fs.readFileSync('/Users/brunowladimir/Downloads/server/images/panela.png');
      await client.sendMessage(serialize, imageBuffer, {
         caption: 'Legenda opcional para a imagem'
     }).then((message) => {
