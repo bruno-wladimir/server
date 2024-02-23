6ade0b06b98c0b33bb94c7f405cf07a3ab2c557e
Bruno Wladimir <brunowladimir@MacBook-Air-de-Bruno.local>
2024-02-22 23:23:26 -0300

    bugs

--- routes/loja.js
+++ routes/loja.js
@@ -615,7 +615,9 @@ console.log("numero preparado"+ serialize)
 
       // await client.sendMessage(serialize, media);
 
-      const mensagemComLink = `*🎉 Olá! 🎉*\n\n Você recebeu esta mensagem por ter comprado na ${nome_loja} 🎉 \nCompartilhe sua opinião e nos ajude a melhorar (clique no link)👇${link}\n\nSua respostá é anônima. A loja não tem acesso aos seus dados.` ;
+      const mensagemComLink = `*🎉 Olá! 🎉*\n\n você recebeu esta mensagem por ter comprado na ${nome_loja} 🎉 \n
+      Compartilhe sua opinião e nos ajude a melhorar *(clique no link)* 👇
+      ${link}\n\nSua respostá é anônima. A loja não tem acesso aos seus dados.` ;
 
 
       //await client.sendMessage( serialize,mensagemComLink);
