12a0638c4ab72ab50d17f6f6bfd7a0ad37b2dd5b
Bruno Wladimir <brunowladimir@MacBook-Air-de-Bruno.local>
2024-02-24 18:53:56 -0300

    altarcao de mensagem

--- models/message_agendamento.js
+++ models/message_agendamento.js
@@ -1,14 +1,15 @@
 const  mongoose = require('mongoose');
 const moment = require('moment-timezone');
 moment.tz.setDefault('America/Sao_Paulo');
+const data = moment();
 
 const Message_agendamento = mongoose.model('Message_agendamento',{
-    _numero: {
+    _numero: String,
+    mensagemComLink: String,
+    timestamp: {
         type: Date,
         default: () => moment().toDate()
       },
-    mensagemComLink: String,
-    timestamp: Date,
     email:String
 
 })
