2114b31349619973219eb39dd0e36e2057456004
Bruno Wladimir <brunowladimir@MacBook-Air-de-Bruno.local>
2024-02-24 11:10:35 -0300

    altarcao de mensagem

--- models/message_agendamento.js
+++ models/message_agendamento.js
@@ -2,7 +2,7 @@ const  mongoose = require('mongoose');
 
 
 const Message_agendamento = mongoose.model('Message_agendamento',{
-    serialize: String,
+    _numero: String,
     mensagemComLink: String,
     timestamp: Date,
     email:String
